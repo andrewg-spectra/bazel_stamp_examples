40854f548a15778c839f7c12d5c3fc2206da03be
andrewg <andrewg@spectralogic.com>
2020-05-07 17:09:37 -0600

    Do something interesting for version

--- 1-go/version.sh
+++ 1-go/version.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 INITIAL_COMMIT_HASH=`git rev-list --max-parents=0 HEAD`
-echo BUILD_VERSION=`git log -1 --format=%at $INITIAL_COMMIT_HASH`
+INITIAL_COMMIT_TIMESTAMP=`git log -1 --format=%at $INITIAL_COMMIT_HASH`
+CURRENT_TIMESTAMP=`date +%s`
+echo BUILD_VERSION $(($CURRENT_TIMESTAMP-$INITIAL_COMMIT_TIMESTAMP))
